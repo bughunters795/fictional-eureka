54651c39d7ef5189d81aad6d33645a31a1b6f93f
bughunters795 <bughunter795@gmail.com>
2025-06-12 03:20:07 +0500

    todo-app

--- consoleapp.py
+++ consoleapp.py
@@ -1,22 +1,30 @@
 # todo-app console projects 
 
 #add feature :
+tasks = []
 
 def add():
-    tasks = []
     while True:
             a = input("Enter your task's:\n")
             if(a== 'done' or a == 'exit' or a== 'clear'):
                 print('no task added')
-                print(tasks)
+                # print(tasks)
+                show()
                 break
             else:
                 tasks.append(a)
-                print(tasks)
-                print('task added  ')
+                # print(tasks)
+                # print('task added  ')
+                # return tasks
+                show()
+
+def show():
+        if(tasks == '' ):
+              print("No tasks found.")
+        else:
+            print("Your Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
 
 add()
 print("thank's for comming ")
-
-
-
